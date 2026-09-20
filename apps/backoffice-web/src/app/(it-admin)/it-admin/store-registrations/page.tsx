95f872b8c0b8714fda0a09866657905883fa0cb9
SST-CRUD <sstdevelopaminno@gmail.com>
2026-09-20 20:27:37 +0700

    feat(it): add store registration review page

---
+++ apps/backoffice-web/src/app/(it-admin)/it-admin/store-registrations/page.tsx
@@ -0,0 +1,7 @@
+import { StoreRegistrationsConsole } from "@/components/it-admin/store-registrations-console";
+
+export const dynamic = "force-dynamic";
+
+export default function StoreRegistrationsPage() {
+  return <StoreRegistrationsConsole />;
+}
