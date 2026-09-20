806c82b1693e8e428de2a55d51aab450ec7d4e9a
SST-CRUD <sstdevelopaminno@gmail.com>
2026-09-21 00:34:46 +0700

    fix(it): accept seeded Starter package UUID in store provisioning

--- apps/backoffice-web/src/lib/services/it-admin/store-provisioning-service.ts
+++ apps/backoffice-web/src/lib/services/it-admin/store-provisioning-service.ts
@@ -67,7 +67,7 @@ function normalizeEmail(value: unknown) {
 }
 
 function isUuid(value: string) {
-  return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(value);
+  return /^[0-9a-f]{8}-(?:[0-9a-f]{4}-){3}[0-9a-f]{12}$/i.test(value);
 }
 
 function isEmail(value: string) {
